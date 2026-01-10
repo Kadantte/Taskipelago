9b4fc7a534f5eeb0dfea2455ce3e49c7f49cde15
Barret Glass <219175183+RedMonkey36@users.noreply.github.com>
2026-01-10 11:40:20 -0600

    Added preqreq options

--- custom_worlds/taskipelago/options.py
+++ custom_worlds/taskipelago/options.py
@@ -14,6 +14,22 @@ class Rewards(OptionList):
     default: List[str] = []
 
 
+class TaskPrereqs(OptionList):
+    """
+    List to show task preqreqs, entries formatted:
+    ""
+    "1"             requires 1
+    "1, 2, 5"       requires 1, 2, 5
+    """
+    display_name = "Task Prereqs"
+    default: List[str] = []
+
+
+class LockPreqreqs(Toggle):
+    display_name = "Lock Tasks Behind Prereqs"
+    default = 0
+
+
 class DeathLinkPool(OptionList):
     display_name = "DeathLink Task Pool"
     default: List[str] = []
@@ -24,4 +40,6 @@ class TaskipelagoOptions(PerGameCommonOptions):
     death_link: DeathLink
     tasks: Tasks
     rewards: Rewards
+    task_prereqs: TaskPrereqs
+    lock_prereqs: LockPreqreqs
     death_link_pool: DeathLinkPool
